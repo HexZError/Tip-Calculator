c0a56e6ce80d6ea4c58332d83b31fd9e51b7c8b2
Mr Black <Mr Black@DESKTOP-2E8GEIA>
2025-02-05 23:42:23 +0530

    javascript code change

--- mind.js
+++ mind.js
@@ -4,7 +4,6 @@ let display = document.getElementById('result');
 // console.log(billAmount);
 let result = 0;
 function calculate(){
-    result = billAmount.value * tip.value / 100;
-    let no =  billAmount.value;
-    display.innerHTML = parseInt(result) + parseInt(no);
+    result = parseInt(billAmount.value) * parseInt(tip.value) / 100;
+    display.innerHTML = parseInt(billAmount.value) + result;
 }
